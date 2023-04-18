4c48c7ccb9f45325964df544cf9d3b92c1b48946
NAME <mail@gmail.com>
2023-04-18 08:41:42 -0500

    First commit Excercises Book Spring Security in Action

---
+++ src/main/java/com/security/config/ProjectConfig.java
@@ -0,0 +1,83 @@
+package com.security.config;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+
+
+@Configuration
+public class ProjectConfig 
+            extends WebSecurityConfigurerAdapter {
+
+   /* @Override
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        var userDetailsService = 
+            new InMemoryUserDetailsManager();
+
+        var user = 
+            User.withUsername("john")
+             .password("12345")
+             .authorities("read")
+             .build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    } */
+
+   /* @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+
+    } */
+
+   @Override
+    protected void configure(HttpSecurity http)
+        throws Exception {
+
+        http.httpBasic();
+        http.authorizeRequests()
+         .anyRequest()
+         .authenticated();
+
+      //get resources without authentication 
+       /* http.authorizeRequests()
+            .anyRequest()
+            .permitAll();*/
+    } 
+    
+    /* Define UserDetailsService and PassEncoder*/
+    public void configure(
+        AuthenticationManagerBuilder auth) 
+                throws Exception {
+    
+        var userDetailsService = 
+            new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("john")
+            .password("12345")
+            .authorities("read")
+            .build();
+
+        userDetailsService.createUser(user);
+
+        auth.userDetailsService(userDetailsService)
+         .passwordEncoder(
+             NoOpPasswordEncoder.getInstance());
+
+    }
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+        System.out.println("XXXXXXXXXXXXX TEST XXXXXXXXXXXXXXXXX");
+	}
+
+}
