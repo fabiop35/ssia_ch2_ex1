063ce7d30e464b21d31ec80ca2ae79e6bd3e5d4f
NAME <mail@gmail.com>
2023-04-20 14:40:00 -0500

    3.3.2 Implementing the UserDetailsService contract

--- src/main/java/com/security/config/UserManagementConfig.java
+++ src/main/java/com/security/config/UserManagementConfig.java
@@ -2,13 +2,18 @@ package com.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-  
-import com.security.model.DummyUser;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.List;
+
+//import com.security.model.DummyUser;
+import com.security.model.User;
+import com.security.service.InMemoryUserDetailsService;
 
 @Configuration
 public class UserManagementConfig {
@@ -17,18 +22,23 @@ public class UserManagementConfig {
     @Bean
     public UserDetailsService userDetailsService(){
 
-        var userDetailsService = 
-             new InMemoryUserDetailsManager();
+       // var userDetailsService = 
+         //    new InMemoryUserDetailsManager();
 
        /* var user = User.withUsername("john")
            .password("123")
            .authorities("read")
            .build(); */
-       DummyUser user = new DummyUser();
+       //DummyUser user = new DummyUser();
+       UserDetails u = 
+           new User("bill", "123", "read");
 
-       userDetailsService.createUser(user); 
+       List<UserDetails> users = 
+                            List.of(u);
 
-       return userDetailsService;
+       //userDetailsService.createUser(user); 
+       //return userDetailsService;
+       return new InMemoryUserDetailsService(users);
 
     }
 
